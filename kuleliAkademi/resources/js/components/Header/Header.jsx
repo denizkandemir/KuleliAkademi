fd927e036e66ab434d349939f65909edf2baf0b3
Deniz Kandemir <denizkandemir137@gmail.com>
2026-04-14 17:02:03 +0200

    Add header images and implement slide functionality in Header component

--- kuleliAkademi/resources/js/components/Header/Header.jsx
+++ kuleliAkademi/resources/js/components/Header/Header.jsx
@@ -1,17 +1,46 @@
 import React from "react";
 import "./Header.scss";
 import Slide from "../Slide/Slide";
+import slideImg1 from "../../assets/images/headerImg1.png";
+import slideImg2 from "../../assets/images/headerImg2.png";
+import slideImg3 from "../../assets/images/headerImg3.png";
+import slideImg4 from "../../assets/images/headerImg4.png";
 
 const Header = () => {
 
     const slideImgs = [
         {
-            _id: 1,
-            url:"",
+            id: 1,
+            url: slideImg1,
+            littleTitle:"",
+            mainTitle:"",
+            description:"",
+        },
+
+        {
+            id: 2,
+            url: slideImg2,
+            littleTitle:"",
+            mainTitle:"",
+            description:"",
+        },
+
+        {
+            id: 3,
+            url: slideImg3,
+            littleTitle:"",
+            mainTitle:"",
+            description:"",
+        },
+
+        {
+            id: 4,
+            url: slideImg4,
             littleTitle:"",
             mainTitle:"",
             description:"",
         }
+
     ]
 
     const iconArray = [
@@ -31,9 +60,15 @@ const Header = () => {
     ];
 
     return (
-        <header className="header-container">
+        <header className="header-container">  
             <Slide
-            
+             SlideImgs={slideImgs}
+             container={"header-slide-container"}
+             iconArray={iconArray}
+             id={1}
+             floatingIcons={floatingIcons}  
+             imgClass={"header-slide-img"}
+             openSlide={() => {}}
             />
         </header>
     )
--- kuleliAkademi/resources/js/components/Slide/Slide.jsx
+++ kuleliAkademi/resources/js/components/Slide/Slide.jsx
@@ -14,8 +14,9 @@ import "./Slide.scss";
 import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
 
 
-const Slide = ({ SlideImgs, imgClass, container, id, isSlideOpen , containerRef, openSlide, iconArray, floatingIcons }) => {
+const Slide = ({ SlideImgs, slideImgs, imgClass, container, id, isSlideOpen, containerRef, openSlide, iconArray, floatingIcons }) => {
     const swiperRef = useRef(null);
+    const slides = SlideImgs ?? slideImgs ?? [];
 
     useEffect(() => {
         if (swiperRef.current) {
@@ -27,9 +28,9 @@ const Slide = ({ SlideImgs, imgClass, container, id, isSlideOpen , containerRef,
         <>
             <div className={"slide-page-container"}>
                 <div className={container}>
-                    <div  ref={containerRef} className="slide-content-container">
-                        {SlideImgs && SlideImgs.length > 0 && (
-                            <Swiper 
+                    <div ref={containerRef} className="slide-content-container">
+                        {slides.length > 0 && (
+                            <Swiper
                                 ref={swiperRef}
                                 modules={[Navigation, Pagination, Autoplay, EffectFade]}
                                 centeredSlides={true}
@@ -50,19 +51,44 @@ const Slide = ({ SlideImgs, imgClass, container, id, isSlideOpen , containerRef,
                                     nextEl: `.swiper-button-next-${id}`,
                                     prevEl: `.swiper-button-prev-${id}`,
                                 }}
-                                
+
                             >
-                                {SlideImgs.map((img) => (
-                                    <SwiperSlide key={img._id}>
-                                        <div  className="slide-container">
+                                {slides.map((img) => (
+                                    <SwiperSlide key={img.id}>
+                                        <div className="slide-container">
                                             <img onClick={() => openSlide()} className={imgClass} src={img.url} alt="" />
+                                        </div>
+
+                                        <div className="slide-texts-container">
+                                            <div className="slide-title-description-container">
+                                                <h3 className="slide-little-title">{img.littleTitle}</h3>
+                                                <h2 className="slide-main-title">{img.mainTitle}</h2>
+                                                <p className="slide-description">{img.description}</p>
+                                            </div>
+
+                                            {
+                                                iconArray && iconArray.length > 0 && iconArray.map((icon) => (
+                                                    <div key={icon.id} className="slide-icon-container">
+                                                        <img src={icon.icon} alt={icon.title} className="header-icon-img" />
+                                                    </div>
+                                                ))
+                                            }
+
+                                            {
+                                                floatingIcons && floatingIcons.length > 0 && floatingIcons.map((icon) => (
+                                                    <div key={icon.id} className="slide-floating-icon-container">
+                                                        <img src={icon.icon} alt={icon.title} className="header-icon-img header-floating-icon" />
+                                                    </div>
+                                                ))
+                                            }
                                         </div>
                                     </SwiperSlide>
                                 ))}
+
                                 <div className={`swiper-button-next-${id} slide-button1`}>
                                     <MdKeyboardArrowRight />
                                 </div>
-                                <div  className={`swiper-button-prev-${id} slide-button2`}>
+                                <div className={`swiper-button-prev-${id} slide-button2`}>
                                     <MdKeyboardArrowLeft />
                                 </div>
                                 <div className={`swiper-pagination-${id}`}></div>
