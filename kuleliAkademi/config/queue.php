0199a88d3260c141f565aaa15fd0758829fdb782
Deniz Kandemir <denizkandemir137@gmail.com>
2026-05-11 02:20:57 +0200

    production settings updated

--- kuleliAkademi/config/queue.php
+++ kuleliAkademi/config/queue.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_CONNECTION', 'database'),
+    'default' => env('QUEUE_CONNECTION', 'sync'),
 
     /*
     |--------------------------------------------------------------------------
@@ -103,7 +103,7 @@ return [
     */
 
     'batching' => [
-        'database' => env('DB_CONNECTION', 'sqlite'),
+        'database' => env('DB_CONNECTION'),
         'table' => 'job_batches',
     ],
 
@@ -121,8 +121,8 @@ return [
     */
 
     'failed' => [
-        'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
-        'database' => env('DB_CONNECTION', 'sqlite'),
+        'driver' => env('QUEUE_FAILED_DRIVER', 'file'),
+        'database' => env('DB_CONNECTION'),
         'table' => 'failed_jobs',
     ],
 
